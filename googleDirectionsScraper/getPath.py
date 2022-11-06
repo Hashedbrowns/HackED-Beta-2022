b7da12ffe69ab932fd70fe6e591a60e7e5bbc978
Ryan Bott <bott.ryan@gmail.com>
2022-11-05 20:43:14 -0600

    Removed API Key from getPath and it is in a local file now. See Ryan if you need dev access.

--- googleDirectionsScraper/getPath.py
+++ googleDirectionsScraper/getPath.py
@@ -2,13 +2,20 @@ import googlemaps
 from RoutePoint import RoutePoint
 from datetime import datetime
 import ProjectCustomExceptions
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Actual API key not stored with application. See Ryan for key if needed for local dev.
+GOOGLE_DIRECTIONS_API_KEY = os.getenv('GOOGLE_DIRECTIONS_API_KEY')
 
 def getPath(point1: RoutePoint , point2: RoutePoint):
     '''
     Takes a pair of RoutePoints and gets the Google Directions API response for them.
     If the response is empty, it will raise an error.
     '''
-    gmaps = googlemaps.Client(key='AIzaSyAgMqgptHbdD-oXy0McQQk9XPW6thfo0T4')
+    gmaps = googlemaps.Client(key=GOOGLE_DIRECTIONS_API_KEY)
 
     # Request directions via public transit
     now = datetime.now()
